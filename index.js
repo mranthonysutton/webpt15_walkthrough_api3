9608b6cda4e7ed4b03792cdd43e54c969e60426c
Anthony Sutton <anthony.sutton95@gmail.com>
2020-07-14 20:09:50 -0700

    feat(completed): Guided project completed :beers:

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code away!
+const server = require("./server");
+const PORT = 1337;
+
+server.listen(PORT, () => {
+  console.log(`\n *** Server running at http://localhost:${PORT}... *** \n`);
+});
+
+module.exports = server;
